8ede56bca822d794abde2caf0de28845bfc3abb8
dream <dream@KSH-MacBook-Air.local>
2023-07-12 20:47:38 +0900

    feat: created parts of CommentListView and created CommentDetailView and fixed parts of prompt/views.py

--- comment/views.py
+++ comment/views.py
@@ -19,3 +19,34 @@ class CommentListView(APIView):
         comments = Comment.objects.filter(post_id=prompt_id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        author = request.user
+        prompt_id = request.data.get('prompt')
+        content = request.data.get('content')
+        if not request.user.is_authenticated:
+            return Response({"detail": "Authentication credentials not provided"}, status=status.HTTP_401_UNAUTHORIZED)
+        if not prompt_id or not content:
+            return Response({"detail": "missing fields ['prompt', 'content']"}, status=status.HTTP_400_BAD_REQUEST)
+        if not Prompt.objects.filter(id=prompt_id).exists():
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        comment = Comment.objects.create(post_id=prompt_id, author=author, content=content)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+class CommentDetailView(APIView):
+    def delete(self, request, comment_id):
+        if not request.user.is_authenticated:
+            return Response({"detail": "Authentication credentials not provided"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        try:
+            comment = Comment.objects.get(id=comment_id)
+        except:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        if request.user != comment.author:
+            return Response({"detail": "Permission denied"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
