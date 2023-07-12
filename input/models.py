dbf96a10c81d39a0ed5b3fa9e43d01578ffd5a4c
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-07-12 21:37:15 +0900

    feat: initialize input model

--- input/models.py
+++ input/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from  django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Input(models.Model):
+    name = models.CharField(max_length=16)
+    type = models.IntegerField(default=1)
+    options = ArrayField(models.CharField(max_length=8), blank=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.name
