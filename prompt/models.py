cd90dfba21cb9743793260f53bdd85af488f4b03
eojin03 <elena1014@naver.com>
2023-07-12 17:30:27 +0900

    feat: add account

--- prompt/models.py
+++ prompt/models.py
@@ -8,6 +8,11 @@ class Prompt(models.Model):
     description = models.CharField(max_length=64)
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
+    #like_users = models.ManyToManyField(User, blank=True, related_name='liked_prompts', through='Like')
 
     def __str__(self):
         return self.title
+    
+class Like(models.Model):
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    prompt = models.ForeignKey(Prompt, on_delete=models.CASCADE)
