7743caaa377d930f23b1ea39fd5266c3681b435c
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-07-13 21:24:44 +0900

    feat: input url initialize

---
+++ input/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import InputDetailView, InputListView
+
+app_name = 'input'
+urlpatterns = [
+    path("", InputListView.as_view()),
+    path("<int:input_id>/", InputDetailView.as_view()),
+    path("<int:prompt_id>/", InputDetailView.as_view())
+]
