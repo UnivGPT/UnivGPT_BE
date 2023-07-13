d4493ffff65560e3aee174b159a969820127b8e3
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-07-13 23:22:27 +0900

    feat: option post view

--- option/views.py
+++ option/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Option
 from prompt.models import Prompt
@@ -17,20 +18,22 @@ from .serializers import OptionSerializer
 #         # comments = Comment.objects.filter(prompt_id=prompt_id)
 #         # serializer = CommentSerializer(comments, many=True)
 #         # return Response(serializer.data, status=status.HTTP_200_OK)
+
+class OptionListView(APIView):
     
-#     def post(self, request):
-#         author = request.user
-#         prompt_id = request.data.get('prompt')
-#         content = request.data.get('content')
-#         if not request.user.is_authenticated:
-#             return Response({"detail": "Authentication credentials not provided"}, status=status.HTTP_401_UNAUTHORIZED)
-#         if not prompt_id or not content:
-#             return Response({"detail": "missing fields ['prompt', 'content']"}, status=status.HTTP_400_BAD_REQUEST)
-#         if not Prompt.objects.filter(id=prompt_id).exists():
-#             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
-#         comment = Comment.objects.create(prompt_id=prompt_id, author=author, content=content)
-#         serializer = CommentSerializer(comment)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def post(self, request):
+        name = request.data.get('name')
+        input_id = request.GET.get('input')
+        if not request.user.is_authenticated:
+          return Response({"detail": "Authentication credentials not provided"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        if not name:
+            return Response({"detail": "missing fields ['name']"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        option = Option.objects.create(name=name, input=input_id)
+        serializer = OptionSerializer(option)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
     
 # class OptionDetailView(APIView):
 #     def delete(self, request, comment_id):
