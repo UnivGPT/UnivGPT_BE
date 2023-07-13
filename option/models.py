fa4150592cc283e225a539010be0615c9ac66732
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-07-13 23:22:14 +0900

    feat: connect input with options model

--- option/models.py
+++ option/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+from input.models import Input
+
 # Create your models here.
 class Option(models.Model):
     name = models.CharField(max_length=16)
+    input = models.ForeignKey(Input, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
