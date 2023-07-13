ecbc0c1b226f6ba866e6dc0dc54b8b86755054a7
sehyeon7 <127166536+sehyeon7@users.noreply.github.com>
2023-07-13 21:15:06 +0900

    Update models.py
    Update models.py

--- prompt/models.py
+++ prompt/models.py
@@ -12,9 +12,9 @@ class Prompt(models.Model):
     view = models.IntegerField(default=0)
     created_at = models.DateTimeField(default=timezone.now)
     like_users = models.ManyToManyField(User, blank=True, related_name='liked_prompts', through='Like')
-    author = models.ForeignKey(User, blank=True, related_name='like_prompt', through='Like')
+    author = models.ForeignKey(User, related_name='like_prompt', through='Like')
     category = models.ManyToManyField(Category, blank=True, related_name='prompts')
-    input = models.ForeignKey(Input, blank=True, related_name='prompt_inputs', on_delete=models.CASCADE)
+    
 
     def __str__(self):
         return self.title
