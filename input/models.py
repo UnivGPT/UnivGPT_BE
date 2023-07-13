d54396d47340f17d257c4b319a92355b34b6fd9b
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-07-13 15:58:24 +0900

    feat: connect prompt-input model

--- input/models.py
+++ input/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from  django.contrib.postgres.fields import ArrayField
 
+from prompt.models import Prompt
+
 # Create your models here.
 class Input(models.Model):
+    prompt = models.ForeignKey(Prompt, on_delete=models.CASCADE)
     name = models.CharField(max_length=16)
     type = models.IntegerField(default=1)
     options = ArrayField(models.CharField(max_length=8), blank=True)
