409c06bd30b9782a0ac40f71b106aaa2625622dc
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-07-13 23:24:46 +0900

    feat: option get view

--- option/views.py
+++ option/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Option
-from prompt.models import Prompt
+from input.models import Input
 from .serializers import OptionSerializer
 
 # Create your views here.
@@ -20,6 +20,15 @@ from .serializers import OptionSerializer
 #         # return Response(serializer.data, status=status.HTTP_200_OK)
 
 class OptionListView(APIView):
+    def get(self, request):
+        input_id = request.GET.get('input')
+        if not input_id:
+            return Response({"detail": "missing fields ['prompt']"}, status=status.HTTP_400_BAD_REQUEST)
+        if not Input.objects.filter(id=input_id).exists():
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        options = Option.objects.filter(input=input_id)
+        serializer = OptionSerializer(options, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
         name = request.data.get('name')
