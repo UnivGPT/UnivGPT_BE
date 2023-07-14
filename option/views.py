1567f5a379d016f406acfb4578e558284409cbe7
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-07-15 00:58:03 +0900

    fix: fix option api

--- option/views.py
+++ option/views.py
@@ -12,7 +12,7 @@ from .serializers import OptionSerializer
 
 class OptionListView(APIView):
     def get(self, request):
-        input_id = request.GET.get('input')
+        input_id = request.data.get('input')
         if not input_id:
             return Response({"detail": "missing fields ['input']"}, status=status.HTTP_400_BAD_REQUEST)
         if not Input.objects.filter(id=input_id).exists():
@@ -23,14 +23,27 @@ class OptionListView(APIView):
     
     def post(self, request):
         name = request.data.get('name')
-        input_id = request.GET.get('input')
+        input_id = request.data.get('input')
         if not request.user.is_authenticated:
           return Response({"detail": "Authentication credentials not provided"}, status=status.HTTP_401_UNAUTHORIZED)
         
         if not name:
             return Response({"detail": "missing fields ['name']"}, status=status.HTTP_400_BAD_REQUEST)
         
-        option = Option.objects.create(name=name, input=input_id)
+        user = request.user
+        
+        input = Input.objects.get(id=input_id)
+
+        prompt_id = input.prompt.id
+        prompt = Prompt.objects.get(id=prompt_id)
+        
+        if user != prompt.author:
+            return Response({"detail": "not the author of this prompt"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        if input.type != 0:
+            return Response({"detail": "invalid input type"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        option = Option.objects.create(name=name, input=input)
         serializer = OptionSerializer(option)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
@@ -46,9 +59,15 @@ class OptionDetailView(APIView):
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
         user = request.user
-        prompt = Prompt.objects.get(id=user.id)
+
+        input_id = option.input.id
+        input = Input.objects.get(id=input_id)
+
+        prompt_id = input.prompt.id
+        prompt = Prompt.objects.get(id=prompt_id)
+        
         if user != prompt.author:
-            return Response({"detail": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({"detail": "not the author of this prompt"}, status=status.HTTP_401_UNAUTHORIZED)
         
         option.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -57,6 +76,20 @@ class OptionDetailView(APIView):
         if not request.user.is_authenticated:
           return Response({"detail": "Authentication credentials not provided"}, status=status.HTTP_401_UNAUTHORIZED)
         option = Option.objects.get(id=option_id)
+
+        user = request.user
+
+        input_id = option.input.id
+        input = Input.objects.get(id=input_id)
+
+        prompt_id = input.prompt.id
+        prompt = Prompt.objects.get(id=prompt_id)
+        
+        if user != prompt.author:
+            return Response({"detail": "not the author of this prompt"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        if request.data['input'] != input_id:
+            return Response({"detail": "input id should not be changed."}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = OptionSerializer(option, data=request.data, partial=True)
         if not serializer.is_valid():
