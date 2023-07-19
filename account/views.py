752e30e7b1807d0fd1033398b9d00f982b9d7ecc
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-07-19 22:38:48 +0900

    fix: fix naver login view (add user to db)

--- account/views.py
+++ account/views.py
@@ -5,6 +5,9 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
+import random
+import string
+
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -20,6 +23,11 @@ def set_token_on_response_cookie(user: User) -> Response:
     res.set_cookie('refresh_token', value=str(token))
     res.set_cookie('access_token', value=str(token.access_token))
     return res
+
+def generate_random_string(length=8):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
 
 # Create your views here.   
 class SignupView(APIView):
@@ -84,6 +92,7 @@ class UserInfoView(APIView):
         return Response(user_serializer.data, status=status.HTTP_200_OK)
 
 class SocialLoginCallbackView(APIView):
+
     def get(self, request):
         state = request.GET.get('state')
         code = request.GET.get('code')
@@ -93,24 +102,36 @@ class SocialLoginCallbackView(APIView):
         
         token_url = f'https://nid.naver.com/oauth2.0/token?client_id={naver_client_id}&client_secret={naver_client_secret}&grant_type=authorization_code&state={state}&code={code}'
         
-        print("================")
         response = requests.get(token_url)
         token_data = response.json()
-        print("============")
-        print("token type data:", type(token_data))
-
 
         headers = {
             'Authorization': f'Bearer {token_data.get("access_token")}'
         }
 
-        print("============")
-        print(type(token_data.get("access_token")))
-
-        print(type(token_data["access_token"]))
         api_url = 'https://openapi.naver.com/v1/nid/me'
         api_response = Response(requests.get(api_url, headers=headers))
 
-        print("api_response", api_response.data.json())
+        user_profile = api_response.data.json().get('response')
+        print(user_profile)
 
-        return (Response(api_response.data.json(), status=status.HTTP_200_OK))
+        email = user_profile.get("email")
+
+        try:
+            user = User.objects.get(email=email)
+            print("existing user using social login")
+            return set_token_on_response_cookie(user)
+        except:
+            username = email.split('@')[0]
+            password = generate_random_string()
+
+            data = {
+                "email": email,
+                "username": username,
+                "password": password,
+            }
+
+            user_serializer = UserSerializer(data=data)
+            if user_serializer.is_valid(raise_exception=True):
+                user = user_serializer.save()
+            return set_token_on_response_cookie(user)
