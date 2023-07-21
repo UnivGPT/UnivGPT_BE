9e5562559439b1943f112baf372e428f4f948707
dream <dream@KSH-MacBook-Air.local>
2023-07-22 00:35:41 +0900

    fix: fixed username error in social login

--- account/views.py
+++ account/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
-import requests, os
+import requests
 
 from django.conf import settings
-from json import JSONDecodeError
-from django.http import JsonResponse
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserProfileSerializer
+from .models import UserProfile
 
 import random
 import string
@@ -16,17 +15,13 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
 
-import random
-import string
-
 def set_token_on_response_cookie(user: User) -> Response:
     token = RefreshToken.for_user(user)
-    user = User.objects.get(username=user)
-    print("Found user", user)
-    user_serializer = UserSerializer(user)
-    res = Response(user_serializer.data, status=status.HTTP_200_OK)
-    res.set_cookie('refresh_token', value=str(token))
-    res.set_cookie('access_token', value=str(token.access_token))
+    user_profile = UserProfile.objects.get(user=user)
+    user_profile_serializer = UserProfileSerializer(user_profile)
+    res = Response(user_profile_serializer.data, status=status.HTTP_200_OK)
+    res.set_cookie('refresh_token', value=str(token), samesite='None', secure=True)
+    res.set_cookie('access_token', value=str(token.access_token), samesite='None', secure=True)
     return res
 
 def generate_random_string(length=8):
@@ -38,13 +33,17 @@ def generate_random_string(length=8):
 class SignupView(APIView):
     def post(self, request):
         email = request.data.get('email')
-        username = email.split('@')[0]  # 이메일에서 @ 앞부분을 사용하여 사용자 이름 생성
+        username = request.data.get('username')
+        socials = request.data.get('socials')
 
-        # request.data에 username 추가
-        request.data['username'] = username
         user_serializer = UserSerializer(data=request.data)
         if user_serializer.is_valid(raise_exception=True):
             user = user_serializer.save()
+
+        user_profile = UserProfile.objects.create(
+                    user=user,
+                    socials=socials
+        )    
         return set_token_on_response_cookie(user)
 
     
@@ -52,11 +51,12 @@ class SigninView(APIView):
     def post(self, request):
         try:
             user = User.objects.get(
-                email = request.data['email'],
+                username = request.data['username'],
                 password = request.data['password']
             )
         except:
-            return Response({"detail": "이메일 또는 비밀번호를 확인해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"detail": "사용자 이름 또는 비밀번호를 확인해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+        user_profile = UserProfile.objects.get(user=user)
         return set_token_on_response_cookie(user)
 
 class LogoutView(APIView):
@@ -91,157 +91,186 @@ class UserInfoView(APIView):
         user_serializer = UserSerializer(user, data=request.data, partial=True)
         if request.data['email'] != user.email:
             return Response({"detail": "email should not be changed."}, status=status.HTTP_400_BAD_REQUEST)
+        if request.data['username'] != user.username:
+            return Response({"detail": "username should not be changed."}, status=status.HTTP_400_BAD_REQUEST)
         if not user_serializer.is_valid(raise_exception=True):
             return Response({"detail": "user data validation error"}, status=status.HTTP_400_BAD_REQUEST)
         user_serializer.save()
         return Response(user_serializer.data, status=status.HTTP_200_OK)
 
 class SocialLoginCallbackView(APIView):
-
     def get(self, request):
+        socials = int(request.GET.get('socials'))
         state = request.GET.get('state')
         code = request.GET.get('code')
 
-        naver_client_id = settings.NAVER_CLIENT_ID
-        naver_client_secret = settings.NAVER_SECRET_KEY
+        if socials == 1:
+            naver_client_id = settings.NAVER_CLIENT_ID
+            naver_client_secret = settings.NAVER_SECRET_KEY
         
-        token_url = f'https://nid.naver.com/oauth2.0/token?client_id={naver_client_id}&client_secret={naver_client_secret}&grant_type=authorization_code&state={state}&code={code}'
+            token_url = f'https://nid.naver.com/oauth2.0/token?client_id={naver_client_id}&client_secret={naver_client_secret}&grant_type=authorization_code&state={state}&code={code}'
         
-        response = requests.get(token_url)
-        token_data = response.json()
+            response = requests.get(token_url)
+            token_data = response.json()
 
-        headers = {
-            'Authorization': f'Bearer {token_data.get("access_token")}'
-        }
-
-        api_url = 'https://openapi.naver.com/v1/nid/me'
-        api_response = Response(requests.get(api_url, headers=headers))
-
-        user_profile = api_response.data.json().get('response')
-        print(user_profile)
-
-        email = user_profile.get("email")
-
-        try:
-            user = User.objects.get(email=email)
-            print("existing user using social login")
-            return set_token_on_response_cookie(user)
-        except:
-            username = email.split('@')[0]
-            password = generate_random_string()
-
-            data = {
-                "email": email,
-                "username": username,
-                "password": password,
+            headers = {
+                'Authorization': f'Bearer {token_data.get("access_token")}'
             }
 
-            user_serializer = UserSerializer(data=data)
-            if user_serializer.is_valid(raise_exception=True):
-                user = user_serializer.save()
-            return set_token_on_response_cookie(user)
-        api_url = 'https://openapi.naver.com/v1/nid/me'
-        api_response = Response(requests.get(api_url, headers=headers))
+            api_url = 'https://openapi.naver.com/v1/nid/me'
+            api_response = Response(requests.get(api_url, headers=headers))
 
-        return (Response(api_response.data.json(), status=status.HTTP_200_OK))
+            user_profile = api_response.data.json().get('response')
+            print(user_profile)
 
-class KakaoLoginCallbackView(APIView):
-    def get(self, request):
-        code = request.GET.get('code')
+            email = user_profile.get("email")
 
-        kakao_client_id = settings.KAKAO_CLIENT_ID
-        kakao_client_secret = settings.KAKAO_SECRET_KEY
-        kakao_redirect_uri = settings.KAKAO_REDIRECT_URI
-        
-        token_url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={kakao_client_id}&client_secret={kakao_client_secret}&code={code}&redirect_uri={kakao_redirect_uri}'
-        
-        response = requests.get(token_url)
-        token_data = response.json()
+            try:
+                users_1 = User.objects.filter(socials=1)
+                user = users_1.get(username=username)
+                user = User.objects.get(email=email)
+                print("existing user using social login")
+                return set_token_on_response_cookie(user)
+            except:
+                # username = email.split('@')[0]
+                password = generate_random_string()
 
-      
+                data = {
+                    "email": email,
+                    "username": username,
+                    "password": password,
+                }
 
+                user_serializer = UserSerializer(data=data)
+                if user_serializer.is_valid(raise_exception=True):
+                    user = user_serializer.save()
 
-        headers = {
-            'Authorization': f'Bearer {token_data.get("access_token")}',
-            'Content-type': "application/x-www-form-urlencoded;charset=utf-8"
-        }
+                user_profile = UserProfile.objects.create(
+                        user=user,
+                        socials=socials
+            )    
+                return set_token_on_response_cookie(user)
+            
+        elif socials == 2:
 
+            kakao_client_id = settings.KAKAO_CLIENT_ID
+            kakao_client_secret = settings.KAKAO_SECRET_KEY
+            kakao_redirect_uri = settings.KAKAO_REDIRECT_URI
+            
+            token_url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={kakao_client_id}&client_secret={kakao_client_secret}&code={code}&redirect_uri={kakao_redirect_uri}'
+            
+            response = requests.get(token_url)
+            token_data = response.json()
 
-        api_url = 'https://kapi.kakao.com/v2/user/me'
-        api_response = Response(requests.get(api_url, headers=headers))
-
-        print("api_response", api_response.data.json())
-
-        user_profile = api_response.data.json().get('kakao_account')
-
-        email = user_profile.get("email")
-
-
-        try:
-            user = User.objects.get(email=email)
-            print("existing user using social login")
-            return set_token_on_response_cookie(user)
-        except:
-            username = email.split('@')[0]
-            password = generate_random_string()
-
-            data = {
-                "email": email,
-                "username": username,
-                "password": password,
+            headers = {
+                'Authorization': f'Bearer {token_data.get("access_token")}',
+                'Content-type': "application/x-www-form-urlencoded;charset=utf-8"
             }
 
-            user_serializer = UserSerializer(data=data)
-            if user_serializer.is_valid(raise_exception=True):
+            api_url = 'https://kapi.kakao.com/v2/user/me'
+            api_response = Response(requests.get(api_url, headers=headers))
+
+            user_profile = api_response.data.json().get('kakao_account')
+            print("================")
+            print(user_profile)
+
+            email = user_profile.get("email")
+            username = user_profile.get("profile").get("nickname")
+
+            same_user_name = User.objects.filter(username=username)
+            if same_user_name.exists():
+                same_user_num = len(same_user_name)
+                username = username+f'@{same_user_num}'
+            else:
+                print("same username does not exist")
+            
+            try:
+                users_2 = User.objects.filter(socials=2)
+                user = users_2.get(username=username)
+                # user = User.objects.get(username=username)
+                return set_token_on_response_cookie(user)
+            except:
+                password = generate_random_string()
+
+                data = {
+                    "email": email,
+                    "username": username,
+                    "password": password,
+                }
+
+                user_serializer = UserSerializer(data=data)
+                user_serializer.is_valid(raise_exception=True)
                 user = user_serializer.save()
-            return set_token_on_response_cookie(user)
+                user_profile = UserProfile.objects.create(
+                    user=user,
+                    socials=socials
+                )    
+                return set_token_on_response_cookie(user)
 
+        elif socials == 3:
+            print("=====================")
+            print("social is 3")
+            client_id = settings.GOOGLE_CLIENT_ID
+            client_secret = settings.GOOGLE_SECRET
+            redirect_uri = settings.GOOGLE_REDIRECT_URI
+            
+            token_req = requests.post(f"https://oauth2.googleapis.com/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={redirect_uri}")
+            token_req_json = token_req.json()
+            # error = token_req_json.get("error")
 
+            # if error is not None:
+            #     raise JSONDecodeError(error)
+            
+            access_token = token_req_json.get('access_token')
 
-class GoogleLoginView(APIView):
-    def get(self, request):
-        client_id = settings.GOOGLE_CLIENT_ID
-        client_secret = settings.GOOGLE_SECRET
-        code = request.GET.get('code')
-        
-        token_req = requests.post(f"https://oauth2.googleapis.com/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_CALLBACK_URI}")
-        token_req_json = token_req.json()
-        # error = token_req_json.get("error")
-
-        # if error is not None:
-        #     raise JSONDecodeError(error)
-        
-        access_token = token_req_json.get('access_token')
-
-        user_data_req = requests.get("https://www.googleapis.com/oauth2/v1/userinfo",
-            headers={"Authorization": f"Bearer {access_token}"},
-        )
-        user_data_json = user_data_req.json()
-        email = user_data_json.get('email')
-
-        try:
-            user = User.objects.get(email=email)
-            refresh = RefreshToken.for_user(user)
-            refresh["email"] = user.email
-            return Response(
-                {
-                    "refresh": str(refresh),
-                    "access": str(refresh.access_token),
-                },
-                status=status.HTTP_200_OK
+            user_data_req = requests.get("https://www.googleapis.com/oauth2/v1/userinfo",
+                headers={"Authorization": f"Bearer {access_token}"},
             )
+            user_data_json = user_data_req.json()
+            print(user_data_json)
+            email = user_data_json.get('email')
+            username = user_data_json.get('name')
 
-        except:
-            user = User.objects.create_user(email=email)
-            user.set_unusable_password()
-            user.save()
-            refresh = RefreshToken.for_user(user)
-            refresh["email"] = user.email
-            return Response(
-                {
-                    "refresh": str(refresh),
-                    "access": str(refresh.access_token),
-                },
-                status=status.HTTP_200_OK
-            )
+            same_user_name = User.objects.filter(username=username)
+            if same_user_name.exists():
+                print("same username exists")
+                same_user_num = len(same_user_name)
+                print(same_user_num)
+                username = username+f'@{same_user_num}'
+                print(username)
+            else:
+                print("same username does not exist")
 
+            try:
+                users_3 = User.objects.filter(socials=3)
+                user = users_3.get(username=username)
+                # user = User.objects.get(username=username)
+                print("existing user using social login")
+                return set_token_on_response_cookie(user)
+            
+            except:
+                password = generate_random_string()
+
+                print(username)
+                data = {
+                    "email": email,
+                    "username": username,
+                    "password": password,
+                }
+                
+                user_serializer = UserSerializer(data=data)
+                # if user_serializer.is_valid(raise_exception=True):
+                try:
+                    if user_serializer.is_valid(raise_exception=True):
+                        print("user is valid")
+
+                        user = user_serializer.save()
+                except:
+                    errors = user_serializer.errors
+                    print(errors)
+
+                user_profile = UserProfile.objects.create(
+                        user=user,
+                        socials=socials
+                )    
+                return set_token_on_response_cookie(user)
