238ecb6c81b10a7f3361a32eabada4457d4ade51
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-07-22 16:59:34 +0900

    feat: initialize GPT view

--- prompt/urls.py
+++ prompt/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import PromptListView, PromptDetailView, LikeView
+from .views import PromptListView, PromptDetailView, LikeView, GPTView
 
 app_name = 'prompt'
 urlpatterns = [
     path("", PromptListView.as_view()),
     path("<int:prompt_id>/", PromptDetailView.as_view()),
-    path("<int:prompt_id>/like/", LikeView.as_view())
+    path("<int:prompt_id>/like/", LikeView.as_view()),
+    path("gpt/", GPTView.as_view())
 ]
